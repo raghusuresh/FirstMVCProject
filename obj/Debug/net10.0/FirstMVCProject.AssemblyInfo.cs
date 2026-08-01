345bc0deac534beaa020b75c35a8cc30f00f6c8b
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2026-08-01 18:03:00 +0530

    Login and Register complted

--- obj/Debug/net10.0/FirstMVCProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FirstMVCProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMVCProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff6705fa939ee080e5a5e6090d911fe9a426c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMVCProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMVCProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
