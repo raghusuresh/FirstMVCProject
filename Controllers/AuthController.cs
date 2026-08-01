aff6705fa939ee080e5a5e6090d911fe9a426c9f
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2026-08-01 17:11:36 +0530

    Intial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVCProject.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+        {
+        
+
+        public DbSet<User> Users { get; set; }
+    }
+}
