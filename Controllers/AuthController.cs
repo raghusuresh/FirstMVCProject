7b2c9483b549700dd9e0ce7885652f5b433d263a
raghusuresh <raghusuresh9604022654@gmail.com>
2026-08-01 20:13:56 +0530

    Created a user dto, need to handle the existing user logic

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstMVCProject.Data;
+using FirstMVCProject.Dto;
+using FirstMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstMVCProject.Controllers
 {
-    public class AuthController : Controller
+    public class AuthController(AppDbContext context) : Controller()
     {
         public IActionResult Login()
         {
@@ -12,6 +16,34 @@ namespace FirstMVCProject.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateUser(UserDto dto)
+         {
+            var ExistingUser = await context.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+
+            if (ExistingUser == null)
+            {
+
+                var user = new User
+                {
+                    Email = dto.Email,
+                    Password = dto.Password,
+                    Username = dto.Username
+                };
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                ModelState.AddModelError("Email", "Email already exists");
+                Console.WriteLine("Email already exists");
+            }
+            return RedirectToAction("Login");
+
+
+
         }
     }
 }
